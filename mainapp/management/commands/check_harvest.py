42607e7785c160d3ea2d634fff64d3c7a2225872
danielkimassai <d.kakai@kokonetworks.com>
2019-11-13 16:23:46 +0300

    updates

--- mainapp/management/commands/check_harvest.py
+++ mainapp/management/commands/check_harvest.py
@@ -7,7 +7,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         today = str(datetime.date.today())
-        fields = Field.objects.filter(data__harvestdate__lte=today)
+        fields = Field.objects.filter(data__harvestdate__lte=today,status='not_ready')
         for field in fields:
         	field.status = 'ready'
         	field.save()
