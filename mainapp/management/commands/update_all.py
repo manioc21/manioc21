1fcd8920f8be73783708e40c9d74f3e6ba158066
daniel kakai <danielkakai3@gmail.com>
2020-04-07 21:22:12 +0300

    commands update

--- mainapp/management/commands/update_all.py
+++ mainapp/management/commands/update_all.py
@@ -2,6 +2,13 @@ from django.core.management.base import BaseCommand
 from mainapp.apis import *
 from mainapp.models import *
 
+def check_data(local_data,ona_data):
+    ids = [data['_id'] for data in ona_data]
+    for item in local_data:
+        if item.data['_id'] not in ids:
+            item.delete()
+    return True
+
 
 class Command(BaseCommand):
     help = 'Update pests'
@@ -10,49 +17,35 @@ class Command(BaseCommand):
         #fields
         fields_ona = get_fields()
         fields_local = Field.objects.all()
-        for field in fields_local:
-            if field.data not in fields_ona:
-                field.delete()
+        check_data(fields_local,fields_ona)
         self.stdout.write("finished fields")
         #farmers
         fields_ona = get_farmers()
         fields_local = Farmer.objects.all()
-        for field in fields_local:
-            if field.data not in fields_ona:
-                field.delete()
+        check_data(fields_local,fields_ona)
         self.stdout.write("finished farmers")
         #pests
         fields_ona = get_pests()
         fields_local = Pest.objects.all()
-        for field in fields_local:
-            if field.data not in fields_ona:
-                field.delete()
+        check_data(fields_local,fields_ona)
         self.stdout.write("finished pests")
         #sales
         fields_ona = get_sales()
         fields_local = Sales.objects.all()
-        for field in fields_local:
-            if field.data not in fields_ona:
-                field.delete()
+        check_data(fields_local,fields_ona)
         self.stdout.write("finished sales")
         #stock
         fields_ona = get_stock()
         fields_local = Stock.objects.all()
-        for field in fields_local:
-            if field.data not in fields_ona:
-                field.delete()
+        check_data(fields_local,fields_ona)
         self.stdout.write("finished stock")
         #supplies
         fields_ona = get_supplies()
         fields_local = Supply.objects.all()
-        for field in fields_local:
-            if field.data not in fields_ona:
-                field.delete()
+        check_data(fields_local,fields_ona)
         self.stdout.write("finished supplies")
         #transformed products
         fields_ona = get_transformedproducts()
         fields_local = TransformedProduct.objects.all()
-        for field in fields_local:
-            if field.data not in fields_ona:
-                field.delete()
+        check_data(fields_local,fields_ona)
         self.stdout.write("finished transformed products")
